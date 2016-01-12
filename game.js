971346c7b222a3aeb492c3334e6dceec06fcb253
Serina Tsang <serinat@uw.edu>
2016-01-12 14:51:14 -0800

    add changes

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ if (question1 ==="yes" || question1 ==="y" || question1 ==="YES" || question1 ==
 } else if (question1 === "no" || question1 === "n" || question1 === "NO" || question1 === "N") {
   console.log(question1 + " " + "is incorrect");
   alert(question1 + " " + "is incorrect");
-}
+};
 
 var question2 = prompt("Do I have arachnaphobia?")
   console.log("This is my second question variable" + " " + question2);
@@ -24,7 +24,7 @@ if (question2 === "yes" || question2 === "y" || question2 === "YES" || question2
 } else if (question2 === "no" || question2 === "n" || question2 ==="NO" || question2 ==="N") {
   console.log(question2 + " " + "is incorrect");
   alert(question2 + " " + "is incorrect");
-}
+};
 
 var question3 = prompt("Do I have two cats?")
   console.log("This is my third question variable" + " " + question3);
@@ -35,7 +35,7 @@ if (question3 === "yes" || question3 === "y" || question3 === "YES" || question3
 } else if (question3 === "no" || question3 === "n" || question3 === "NO" || question3 === "N") {
   console.log(question3 + " " + "is correct. I have two dogs");
   alert(question3 + " " + "is correct. I have two dogs");
-}
+};
 
 var question4 = prompt("How young am I?")
   console.log("This is my fourth question variable" + " " + question4);
@@ -43,7 +43,7 @@ var question4 = prompt("How young am I?")
 if (question4 === "37") {
   console.log("You got it!");
   alert("You got it!");
-}
+};
 
 if (question4 < "37") {
   console.log("You are too low");
@@ -51,4 +51,4 @@ if (question4 < "37") {
 } else if (question4 > "37") {
   console.log("You are too high");
   alert("You are too high");
-}
+};
