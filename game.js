e63542f52480d4d9c4025b00f081727ef114ded0
Serina Tsang <serinat@uw.edu>
2016-01-17 22:32:35 -0800

    fixed merged conflicts of game.js

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var numCorrect = 0;
 
 var ans1 = document.getElementById('a1');
@@ -20,17 +19,6 @@ function game(questions, answers, index) {
     console.log(ques + ' correct');
     els[index].textContent = ques + ' is correct';
     numCorrect++
-=======
-var questions = ["Did I grow up in Seattle?", "Do I have arachnaphobia?", "Do I have two cats?"];
-var answers = ["yes", "yes", "no"];
-var counter = 0;
-
-alert("Welcome to my guessing game. I'm going to ask you a couple of yes or no questions so you can get to know me.");
-
-function updatePage(elId, text) {
-  document.getElementById(elId).textContent = text;
-}
->>>>>>> 873a78e696c424e385ef63cacab2f1cfae3c41db
 
   } else {
     console.log(ques + ' is incorrect');
@@ -38,37 +26,8 @@ function updatePage(elId, text) {
     }
   }
 
-<<<<<<< HEAD
   for (var i = 0; i < questions.length; i++) {
     game(questions[i], answers [i], i);
 }
 
 elCorrect.textContent = "You got " + numCorrect + " out of 4 questions!"
-=======
-function userScore() {
-  updatePage("userScore", "You got " + counter + " out of " + questions.length + " correct!");
-}
-
-
-function game(question, answer) {
-  var ques = prompt(question)
-  console.log("answers to question " + ques);
-  console.log(answer + ": answer");
-  console.log(answer == ques);
-
-  updatePage("question", question);
-  updatePage("answer", answer);
-
-  if(answer == ques) {
-    counter +=1
-    console.log(counter);
-  updatePage("isCorrect", "Congrats! You are correct!  You've guessed " + counter + " out of 3 correct.");
-  } else {
-  updatePage("isCorrect", "Sorry, you are incorrect.");
-  }
-};
-
-  for (var i = 0; i < questions.length; i++) {
-    game(questions[i], answers [i]);
-}
->>>>>>> 873a78e696c424e385ef63cacab2f1cfae3c41db
